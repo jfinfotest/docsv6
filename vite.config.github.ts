14884fe7a77ca529b112d9172e3849e4aef5bd63
Jhon <jfinfocesde@gmail.com>
2025-09-14 20:53:44 -0500

    c2

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -93,17 +93,17 @@ export default defineConfig({
         start_url: '/docsv6/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/docsv6/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/docsv6/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/docsv6/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
